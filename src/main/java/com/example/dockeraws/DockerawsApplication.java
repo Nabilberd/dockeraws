76f872cecfe5539de4be0d10729a058fc80cecf5
Nabil <nlembared@pca.ma>
2020-03-03 20:42:57 +0100

    initial commit : push generated project with health API

---
+++ src/main/java/com/example/dockeraws/DockerawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockeraws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerawsApplication.class, args);
+	}
+
+}
